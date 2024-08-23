8e72550c29e1fb993251ccbe9d591bef7549a08c
zxe <zxe@xidian.com>
2024-08-23 10:29:37 +0800

    h264

---
+++ h264/w-h.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+   FILE *fp;
+   char c[] = "This is runoob";
+   char buffer[20];
+ 
+   /* 打开文件用于读写 */
+   fp = fopen("file.txt", "w+");
+ 
+   /* 写入数据到文件 */
+   fwrite(c, strlen(c) + 1, 1, fp);
+ 
+   /* 查找文件的开头 */
+   fseek(fp, 0, SEEK_SET);
+ 
+   /* 读取并显示数据 */
+   int a = fread(buffer, strlen(c)+1, 1, fp);
+   printf("%s\n", buffer);
+   fclose(fp);
+   printf("%d\n",a);
+   
+   return(0);
+}
